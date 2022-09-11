3ca6a8feb7a3657a06c1b87e111388a313b03404
Hossein Salehi <aerocov@gmail.com>
2022-09-11 20:25:19 +1000

    _wip

--- infrastructure/platform/variables.tf
+++ infrastructure/platform/variables.tf
@@ -2,11 +2,6 @@ variable "location" {
   type    = string
   default = "Australia Southeast"
 }
-
-variable "platform_name" {
-  type = string
-}
-
 variable "platform_env" {
   type    = string
   default = "dev"
