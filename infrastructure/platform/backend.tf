47f434aeb1c2e7ca6bf7d0a2d07ed7c75a03783d
Hossein Salehi <aerocov@gmail.com>
2022-09-11 20:14:31 +1000

    wip

--- infrastructure/platform/backend.tf
+++ infrastructure/platform/backend.tf
@@ -1,8 +1,5 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "hoss_dev_tfstate"
-    storage_account_name = "hossterraformabbtlmzr"
-    container_name       = "tfstate"
-    key                  = "some_app"
+    key = "some_app"
   }
 }
